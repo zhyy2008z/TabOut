6e8b1cc365d1298f29aa60cb0b8aadf5f4a8bba1
zhyy2008z <zhangyuyang@kfckgs.com>
2024-08-17 10:20:34 +0800

    更新版本

--- source.extension.cs
+++ source.extension.cs
@@ -9,9 +9,9 @@ namespace TabOut
     {
         public const string Id = "TabOut.97c51f42-4a79-4529-a3d5-43a249d0e2b8";
         public const string Name = "TabOut";
-        public const string Description = @"仿VS Code TabOut插件";
+        public const string Description = @"仿 VS Code TabOut 插件";
         public const string Language = "en-US";
-        public const string Version = "2.0";
+        public const string Version = "2.1";
         public const string Author = "穿越中的逍遥";
         public const string Tags = "";
     }
