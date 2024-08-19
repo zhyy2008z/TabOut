2ae7b077652f4c8a53b51ef3be4ec312222957f6
zhyy2008z <zhangyuyang@kfckgs.com>
2024-08-19 10:39:45 +0800

    调整扩展语言设置

--- source.extension.cs
+++ source.extension.cs
@@ -10,8 +10,8 @@ namespace TabOut
         public const string Id = "TabOut.97c51f42-4a79-4529-a3d5-43a249d0e2b8";
         public const string Name = "TabOut";
         public const string Description = @"仿 VS Code TabOut 插件";
-        public const string Language = "en-US";
-        public const string Version = "2.4";
+        public const string Language = "zh-CN";
+        public const string Version = "2.4.1";
         public const string Author = "穿越中的逍遥";
         public const string Tags = "";
     }
